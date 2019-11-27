0e58e425a054409793e12980f2b0ae308ef14505
A.Kufeld <kufeldnp@gmail.com>
2019-11-27 19:50:01 +0300

    Квадранты

---
+++ Homework_4_11_If_Number_1/Homework_4_11_If_Number_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4_11_If_Number_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координату Х точки");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координату У точки");
+            var y = double.Parse(Console.ReadLine());
+
+            if (x > 0 & y > 0)
+            {
+                Console.WriteLine("Точка принадлежит первой четверти");
+            }
+            else if (x < 0 & y > 0)
+            {
+                Console.WriteLine("Точка принадлежит второй четверти");
+            }
+            else if (x < 0 & y < 0)
+            {
+                Console.WriteLine("Точка принадлежит третьей четверти");
+            }
+            else if (x > 0 & y < 0)
+            {
+                Console.WriteLine("Точка принадлежит четвёртой четверти");
+            }
+            else if (x == 0 & y != 0)
+            {
+                Console.WriteLine("Точка лежит на оси абсцисс");
+            }
+            else if (y == 0 & y != 0)
+            {
+                Console.WriteLine("Точка лежит на оси ординат");
+            }
+            else
+            {
+                Console.WriteLine("Точка принадлежит началу координат");
+            }
+            Console.ReadKey();
+        }
+    }
+}
