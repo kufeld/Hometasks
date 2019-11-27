d95f21130a61ebfe7f27f1e5386837885b41c2dc
A.Kufeld <kufeldnp@gmail.com>
2019-11-27 19:51:35 +0300

    Значение функции

---
+++ Homework_4_11_If_Number_4/Homework_4_11_If_Number_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4_11_If_Number_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для вычисления значения функции f(x) введите значение аргумента х");
+            var x = double.Parse(Console.ReadLine());
+
+            if (x > 2)
+            {
+                Console.WriteLine("f(x) = 2");
+            }
+            else if (x >= -2 & x <= 2)
+            {
+                Console.WriteLine("f(x) = " + x);
+            }
+            else
+            {
+                Console.WriteLine("f(x) = -2");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
