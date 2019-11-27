ec422b222f60b26345cefee0b659866ff5858ec1
A.Kufeld <kufeldnp@gmail.com>
2019-11-27 19:41:27 +0300

    Метод-функция. Номер 1

---
+++ Homework_14_10_Number_4/Homework_14_10_Number_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_14_10_Number_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = F(2,3,5) + F(5,7,12) + F(11,13,24);
+
+            Console.WriteLine("x = {0:F3}", x);
+
+        }
+        static double F(double x, double y, double z)
+        {
+            var sin1 = Math.Sin(x);
+            var sin2 = Math.Sin(y);
+            return ((sin1 + sin2) / z);
+        }
+    }
+}
