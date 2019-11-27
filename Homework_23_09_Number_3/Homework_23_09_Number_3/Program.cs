0b1d061f067d653954d8a4a19367505cbe46a676
A.Kufeld <kufeldnp@gmail.com>
2019-11-27 19:46:31 +0300

    Математические формулы

---
+++ Homework_23_09_Number_3/Homework_23_09_Number_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_23_09_Number_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result, first, second;
+            Console.WriteLine("Введите любое число х, для которого нужно вычислить значение функции f(x)");
+            double x = double.Parse(Console.ReadLine());
+            first = x * x + 10;
+            second = Math.Sqrt(x * x + 1);
+            result = first / second;
+            Console.Write("Значение функции равно ");
+            Console.WriteLine(result);
+        }
+    }
+}
