10643604973a15ac2d525a629d8a025187940701
A.Kufeld <kufeldnp@gmail.com>
2019-12-15 20:08:29 +0300

    Добавляю домашнее задание

---
+++ ЛогическиеОперации_Номер_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ЛогическиеОперации_Номер_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var x = Input("x");
+            var y = Input("y");
+
+            Console.WriteLine("Точка принадлежит графику " + CheckGraphic(x, y));
+
+            Console.ReadKey();
+        }
+
+        static bool CheckGraphic(double x, double y)
+
+        {
+            return y > -2 && y < 1.5;
+        }
+        static double Input(string name)
+
+        {
+            Console.WriteLine("Введите координаты точки x, y. " + name + " = ");
+            return double.Parse(Console.ReadLine());
+        }
+
+    }
+}
